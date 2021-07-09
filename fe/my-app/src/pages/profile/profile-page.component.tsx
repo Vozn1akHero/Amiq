29ca4bf127c4738e6129cd4ae43230a163a6e7dd
Dmytro Vozniachuk <dmytro.vozniachuk@asseco.pl>
2021-07-09 22:03:31 +0200

    zmiany 09.07

---
+++ fe/my-app/src/pages/profile/profile-page.component.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ProfilePageComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ProfilePageComponent;
